5a1e9bfd32864d87f35e338a781a2b51bec7bd58
An Nguyen Hieu Duc <andy.annguyen3520@gmail.com>
2020-08-11 18:07:14 +0700

    unkown and never

---
+++ app.js
@@ -0,0 +1,14 @@
+var userInput;
+var userName;
+userInput = 5;
+userInput = 'Andy';
+// have to check first w/ unknow type before assign value
+if (typeof userInput === 'string') {
+    userName = userInput;
+}
+function generateError(message, code) {
+    throw { message: message, errorCode: code };
+    // while (true) {} is never type too
+}
+var result = generateError('An error occurred!', 500);
+console.log(result);
---
+++ app.ts
@@ -0,0 +1,19 @@
+let userInput: unknown;
+let userName: string;
+
+userInput = 5;
+userInput = 'Andy';
+
+// have to check first w/ unknow type before assign value
+if (typeof userInput === 'string') {
+  userName = userInput;
+}
+
+function generateError(message: string, code: number): never {
+  throw { message, errorCode: code };
+  // while (true) {} is never type too
+}
+
+const result = generateError('An error occurred!', 500);
+
+console.log(result);
